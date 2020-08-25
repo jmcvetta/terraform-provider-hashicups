0105f71d50e9e1350efd20bb0d64c26e81b5ef94
Jason McVetta <jason.mcvetta@gmail.com>
2020-08-25 20:13:05 +0700

    cleanup

--- hashicups/coffees.go
+++ hashicups/coffees.go
@@ -1,105 +1,100 @@
 package hashicups
 
 import (
-  "context"
-  "fmt"
-  "context"
-  "encoding/json"
-  "fmt"
-  "net/http"
-  "strconv"
-  "time"
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strconv"
+	"time"
 
-  "github.com/hashicorp/terraform-plugin-sdk/v2/diag"
-  "github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
-  "net/http"
-  "time"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
 
 func dataSourceCoffees() *schema.Resource {
-  return &schema.Resource{
-    Schema: coffeeSchema,
-    ReadContext: dataSourceCoffeesRead,
-  }
+	return &schema.Resource{
+		Schema:      coffeeSchema,
+		ReadContext: dataSourceCoffeesRead,
+	}
 }
 
-
 var coffeeSchema = map[string]*schema.Schema{
-  "coffees": &schema.Schema{
-    Type:     schema.TypeList,
-    Computed: true,
-    Elem: &schema.Resource{
-      Schema: map[string]*schema.Schema{
-        "id": &schema.Schema{
-          Type:     schema.TypeInt,
-          Computed: true,
-        },
-        "name": &schema.Schema{
-          Type:     schema.TypeString,
-          Computed: true,
-        },
-        "teaser": &schema.Schema{
-          Type:     schema.TypeString,
-          Computed: true,
-        },
-        "description": &schema.Schema{
-          Type:     schema.TypeString,
-          Computed: true,
-        },
-        "price": &schema.Schema{
-          Type:     schema.TypeInt,
-          Computed: true,
-        },
-        "image": &schema.Schema{
-          Type:     schema.TypeString,
-          Computed: true,
-        },
-        "ingredients": &schema.Schema{
-          Type:     schema.TypeList,
-          Computed: true,
-          Elem: &schema.Resource{
-            Schema: map[string]*schema.Schema{
-              "ingredient_id": &schema.Schema{
-                Type:     schema.TypeInt,
-                Computed: true,
-              },
-            },
-          },
-        },
-      },
-    },
-  },
+	"coffees": &schema.Schema{
+		Type:     schema.TypeList,
+		Computed: true,
+		Elem: &schema.Resource{
+			Schema: map[string]*schema.Schema{
+				"id": &schema.Schema{
+					Type:     schema.TypeInt,
+					Computed: true,
+				},
+				"name": &schema.Schema{
+					Type:     schema.TypeString,
+					Computed: true,
+				},
+				"teaser": &schema.Schema{
+					Type:     schema.TypeString,
+					Computed: true,
+				},
+				"description": &schema.Schema{
+					Type:     schema.TypeString,
+					Computed: true,
+				},
+				"price": &schema.Schema{
+					Type:     schema.TypeInt,
+					Computed: true,
+				},
+				"image": &schema.Schema{
+					Type:     schema.TypeString,
+					Computed: true,
+				},
+				"ingredients": &schema.Schema{
+					Type:     schema.TypeList,
+					Computed: true,
+					Elem: &schema.Resource{
+						Schema: map[string]*schema.Schema{
+							"ingredient_id": &schema.Schema{
+								Type:     schema.TypeInt,
+								Computed: true,
+							},
+						},
+					},
+				},
+			},
+		},
+	},
 }
 
 func dataSourceCoffeesRead(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
-  client := &http.Client{Timeout: 10 * time.Second}
+	client := &http.Client{Timeout: 10 * time.Second}
 
-  // Warning or errors can be collected in a slice type
-  var diags diag.Diagnostics
+	// Warning or errors can be collected in a slice type
+	var diags diag.Diagnostics
 
-  req, err := http.NewRequest("GET", fmt.Sprintf("%s/coffees", "http://localhost:19090"), nil)
-  if err != nil {
-    return diag.FromErr(err)
-  }
+	req, err := http.NewRequest("GET", fmt.Sprintf("%s/coffees", "http://localhost:19090"), nil)
+	if err != nil {
+		return diag.FromErr(err)
+	}
 
-  r, err := client.Do(req)
-  if err != nil {
-    return diag.FromErr(err)
-  }
-  defer r.Body.Close()
+	r, err := client.Do(req)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+	defer r.Body.Close()
 
-  coffees := make([]map[string]interface{}, 0)
-  err = json.NewDecoder(r.Body).Decode(&coffees)
-  if err != nil {
-    return diag.FromErr(err)
-  }
+	coffees := make([]map[string]interface{}, 0)
+	err = json.NewDecoder(r.Body).Decode(&coffees)
+	if err != nil {
+		return diag.FromErr(err)
+	}
 
-  if err := d.Set("coffees", coffees); err != nil {
-    return diag.FromErr(err)
-  }
+	if err := d.Set("coffees", coffees); err != nil {
+		return diag.FromErr(err)
+	}
 
-  // always run
-  d.SetId(strconv.FormatInt(time.Now().Unix(), 10))
+	// always run
+	d.SetId(strconv.FormatInt(time.Now().Unix(), 10))
 
-  return diags
+	return diags
 }
