901202d8f6d375d11e7136f8d8bd662e3b4c49cb
Jason McVetta <jason.mcvetta@gmail.com>
2020-08-27 16:37:21 +0700

    schema

--- hashicups/provider.go
+++ hashicups/provider.go
@@ -7,9 +7,23 @@ import (
 // Provider -
 func Provider() *schema.Provider {
 	return &schema.Provider{
-		ResourcesMap:   map[string]*schema.Resource{},
+		Schema: map[string]*schema.Schema{
+			"username": &schema.Schema{
+				Type:        schema.TypeString,
+				Optional:    true,
+				DefaultFunc: schema.EnvDefaultFunc("HASHICUPS_USERNAME", nil),
+			},
+			"password": &schema.Schema{
+				Type:        schema.TypeString,
+				Optional:    true,
+				Sensitive:   true,
+				DefaultFunc: schema.EnvDefaultFunc("HASHICUPS_PASSWORD", nil),
+			},
+		},
+		ResourcesMap: map[string]*schema.Resource{},
 		DataSourcesMap: map[string]*schema.Resource{
 			"hashicups_coffees": dataSourceCoffees(),
 		},
+		ConfigureContextFunc: providerConfigure,
 	}
 }
